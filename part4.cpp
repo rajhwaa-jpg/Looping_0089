569cc9b95bac5c155e8920908e26cff22261e720
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-03-31 17:23:04 +0700

    Initialize main function and add input prompts for Mathematics and English scores

--- part4.cpp
+++ part4.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
